12e5df99029391da81ccc1e0c5b97042ac75a1f9
soerenpriess <toast357357@googlemail.com>
2026-02-22 10:23:29 +0100

    feat: remove Google AdSense configuration and script loading from main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,6 @@ import App from './App.vue'
 import { useColorMode } from '@vueuse/core'
 import router from './router/_index'
 
-// Load Google AdSense
-const adsenseClientId = import.meta.env.VITE_ADSENSE_CLIENT_ID
-if (adsenseClientId) {
-  const script = document.createElement('script')
-  script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adsenseClientId}`
-  script.async = true
-  script.crossOrigin = 'anonymous'
-  document.head.appendChild(script)
-}
-
 const app = createApp(App)
 
 app.use(router)
